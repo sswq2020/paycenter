ead8dec2fad0eed169be443c8059de1a51209088
shiswq@hletong.com <shiswq@hletong.com>
2019-10-23 18:52:30 +0800

    fix: 🐛 1.根据环境变量判断sid.2.用脚本来判断加载的静态资源

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,12 +1,18 @@
 <template>
-	<HletongLayout v-on:loginOut="loginOut" systemId="153907405736902656">
+	<HletongLayout v-on:loginOut="loginOut" :systemId="systemId">
 		<router-view></router-view>
 	</HletongLayout>
 </template>
 
 <script>
+	const SystemId = process.env.NODE_ENV === 'production' ? "166948708439556096": "153907405736902656"
 	export default {
 		name:'Main',
+		data(){
+			return {
+				systemId:SystemId
+			}
+		},
 		watch: {
 			$route(to,from){
 				// console.log(to);
